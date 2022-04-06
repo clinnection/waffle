a279e9ddc300c729af492531b17ff7cc3542f03c
Bart Kiers <bkiers@gmail.com>
2022-04-06 20:18:32 +0200

    Added demo unit tests

---
+++ src/test/com/clinnection/wf/parser/LexerTest.java
@@ -0,0 +1,16 @@
+package com.clinnection.wf.parser;
+
+import org.junit.Test;
+
+import static com.clinnection.wf.matcher.ProducesToken.producesToken;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static com.clinnection.wf.parser.WfLexer.*;
+
+public class LexerTest {
+
+  @Test
+  public void test() {
+    assertThat("==", producesToken(EQUALS));
+  }
+}
---
+++ src/test/com/clinnection/wf/parser/ParserTest.java
@@ -0,0 +1,14 @@
+package com.clinnection.wf.parser;
+
+import org.junit.Test;
+
+import static com.clinnection.wf.matcher.UsesAllTokens.usesAllTokens;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ParserTest {
+
+  @Test
+  public void test() {
+    assertThat("1 * 2", usesAllTokens("integerExpr"));
+  }
+}
